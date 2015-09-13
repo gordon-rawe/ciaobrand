47df13a5ed22abef824c0c40d74259ed8401fe0d
Gordon Rawe <gordon.tongji@hotmail.com>
2015-09-13 23:55:04 +0800

    somethig

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+ username:"cd486413f4854cf2bbaf23dee4c0e07d",
+ password:"b6cf6fd6232d48f498315b4812d0fd8c",
+ db_host:"mongo.duapp.com",
+ db_name:"PicgMRufZzMqWrLueZuU",
+ db_port:"8908"
+}
+
+module.exports = config;
